6307f6661c82351fe27b9b6dff00cb5b2a3656fc
Nina Leung <nleung320@gmail.com>
2016-01-30 11:38:43 -0600

    modified method to select from already selected results, to pass the multiple criteria test

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,18 +1,11 @@
 class Array
   def where(input_hash)
-    # @results = []
+    @results = self
     input_hash.each do |attribute, search_term|
       if search_term.class == Regexp
-        @results = self.select { |quote| quote[attribute] =~ search_term }
+        @results = @results.select { |quote| quote[attribute] =~ search_term }
       else
-        @results = self.select { |quote| quote[attribute] == search_term }
-      end
-    end
-
-    if input_hash.length > 1
-      @results = @results.select do |quote|
-      quote[input_hash.keys[0]] == input_hash[input_hash.keys[0]] && 
-      quote[input_hash.keys[1]] =~ input_hash[input_hash.keys[1]] 
+        @results = @results.select { |quote| quote[attribute] == search_term }
       end
     end
 
