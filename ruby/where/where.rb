4baf57e95af111c8da73d4fc3b83e5fd41ac3a50
Nina Leung <nleung320@gmail.com>
2016-01-30 09:34:38 -0600

    added conditionals to pass the tests using regular expressions in search terms

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,8 +1,12 @@
 class Array
   def where(input_hash)
     # @results = []
-    input_hash.each do |key, value|
-      @results = self.select { |quote| quote[key] == value }
+    input_hash.each do |attribute, search_term|
+      if search_term.class == Regexp
+        @results = self.select { |quote| quote[attribute] =~ search_term }
+      else
+        @results = self.select { |quote| quote[attribute] == search_term }
+      end
     end
 
     @results
