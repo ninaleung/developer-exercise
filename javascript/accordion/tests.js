e370ab56730cee6980c782f1a62a939ce321e353
Nina Leung <nleung320@gmail.com>
2016-02-09 14:33:15 -0600

    added test number of showing panels initially

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -8,13 +8,17 @@ $(document).ready(function(){
   test( "hello test", function( assert ) {
     assert.ok( 1 == "1", "Passed!" );
   });
-  
+
   test( "number of accordions for ACT example", function( assert ) {
     assert.strictEqual( acc.length, 5, "Yup, 5 accordions!");
   });
 
   test( "At least one accordion", function( assert ) {
     assert.ok( acc.length > 0, "Yup, accordions exist!");
+  });
+
+  test( "Nothing showing on load", function( assert ) {
+    assert.equal( document.getElementsByClassName("show").length, 0, "Yes, no accordions are showing on load!");
   });
 
 });
