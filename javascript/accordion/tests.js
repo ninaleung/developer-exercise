175f45dadff383abd7ddc7ea15796fd8d215b110
Nina Leung <nleung320@gmail.com>
2016-02-09 14:32:32 -0600

    added tests for number of accordions

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
   test("example test", function() {
     ok(true);
   });
-  
+
   test( "hello test", function( assert ) {
     assert.ok( 1 == "1", "Passed!" );
+  });
+  
+  test( "number of accordions for ACT example", function( assert ) {
+    assert.strictEqual( acc.length, 5, "Yup, 5 accordions!");
+  });
+
+  test( "At least one accordion", function( assert ) {
+    assert.ok( acc.length > 0, "Yup, accordions exist!");
   });
 
 });
