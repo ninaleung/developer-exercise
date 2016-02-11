8d4141a88c943750e7f1ee7c5e7d2c89ee67b8ff
Nina Leung <nleung320@gmail.com>
2016-02-11 16:13:22 -0600

    added url to quotesCollection definition

--- javascript/simple_backbone/js/collections/allQuotes.js
+++ javascript/simple_backbone/js/collections/allQuotes.js
@@ -1,5 +1,6 @@
 var app = app || {};
 
 app.quotesCollection = Backbone.Collection.extend({
-  model: app.singleQuote
+  model: app.singleQuote,
+  url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
 });
